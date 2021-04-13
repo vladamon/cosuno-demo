6fc91e9f771f4813666f81730c4595207d049c7a
Vladimir Cutkovic <vladimir.cutkovic@gmail.com>
2021-04-13 21:19:59 +0200

    Fix start scripts Fix seed script Remove unused logic

--- packages/service/company/src/data/seed.js
+++ packages/service/company/src/data/seed.js
@@ -5,13 +5,13 @@ import fs from 'fs';
 const collectionPrefix = null;
 
 export default async function seed(db) {
-  const files = ['cities', 'companies', 'specialties'];
+  //const files = ['cities', 'companies', 'specialties'];
 
-  //const collectionNames = (await db.listCollections().toArray()).map((o) => o.name);
-  const collectionNames = [...files]
+  const collectionNames = (await db.listCollections().toArray()).map((o) => o.name);
+  //const collectionNames = [...files]
 
   return Promise.all(
-    files.map((file) => {
+    collectionNames.map((file) => {
       return (async function () {
         const colName = `${collectionPrefix || ''}${file}`;
         const data = JSON.parse(fs.readFileSync(`${__dirname}/${file}.json`, 'utf8'));
